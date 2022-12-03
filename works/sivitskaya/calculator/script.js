64deb5ccd277b3733c9ee02141d914f2d0440f39
Dmitry Nasonov <gvardopo4ta@gmail.com>
2022-12-03 16:03:35 +0300

    work with average values complete

--- works/sivitskaya/calculator/script.js
+++ works/sivitskaya/calculator/script.js
@@ -1,4 +1,4 @@
-function calc() {
+function createInputs() {
     k = parseInt(document.getElementById("number").value);
     var inputs = document.getElementById("inputs");
     for (i=0; i<k; i++) {
@@ -6,4 +6,17 @@ function calc() {
         inpt.value = i;
         inputs.appendChild(inpt);
     }
+}
+
+function calc() {
+    var divInputs = document.getElementById("inputs");
+    inputs = divInputs.getElementsByTagName('input');
+    var sum = 0;
+    console.log("Собранные поля input и их количество:", inputs, inputs.length);
+    for (let item of inputs) {
+        sum += parseInt(item.value);
+    }
+    var avg = sum/inputs.length;
+    console.log("Средний балл", avg);
+    document.getElementById('result').innerHTML = avg;
 }
